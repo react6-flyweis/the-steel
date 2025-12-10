78b00ae9ce3db188c27abf903be1a72dc62a0ecd
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-10 21:59:58 +0530

    feat: add First Come First Serve banner image to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,20 @@ import HeroSection from "@/components/home/HeroSection";
 import Testimonials from "@/components/home/Testimonials";
 import TrustedBy from "@/components/home/TrustedBy";
 
+import firstComeFirstServeImage from "@/assets/home/first-come-banner.png";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
       <HeroSection />
       <TrustedBy />
       <Testimonials />
+      <Image
+        src={firstComeFirstServeImage}
+        alt="First Come First Serve Banner"
+        className="w-full "
+      />
     </>
   );
 }
