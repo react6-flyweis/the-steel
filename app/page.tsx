5661591e41cbf587cf00974e086bfdcf8559093b
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-15 22:26:15 +0530

    feat: add Header and Footer components to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Testimonials from "@/components/home/Testimonials";
 import TrustedBy from "@/components/home/TrustedBy";
 
 import firstComeFirstServeImage from "@/assets/home/first-come-banner.png";
+import dreamBuildingImg from "@/assets/dream-building-home.png";
+
 import Image from "next/image";
 import Buildings from "@/components/home/Buildings";
 import DesignYourBuilding from "@/components/home/DesignYourBuilding";
@@ -10,10 +12,13 @@ import OurPromise from "@/components/home/OurPromise";
 import BuildSteps from "@/components/home/BuildSteps";
 import BuildingInAction from "@/components/home/BuildingInAction";
 import DreamBuilding from "@/components/home/DreamBuilding";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <HeroSection />
       <TrustedBy />
       <Testimonials />
@@ -27,7 +32,8 @@ export default function Home() {
       <OurPromise />
       <BuildSteps />
       <BuildingInAction />
-      <DreamBuilding />
+      <DreamBuilding image={dreamBuildingImg} />
+      <Footer />
     </>
   );
 }
