0c13835a693b8f18f9d372bae3e5b4f7e2c12aa5
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-15 22:22:21 +0530

    feat: update DreamBuilding component to accept dynamic image prop and enhance layout

--- components/home/DreamBuilding.tsx
+++ components/home/DreamBuilding.tsx
@@ -1,29 +1,33 @@
-import Image from "next/image";
-import dreamBuildingImg from "@/assets/buildings/dream-building.png";
+import Image, { StaticImageData } from "next/image";
+
 import Container from "../Container";
 import { Button } from "../ui/button";
 import { ArrowRight } from "lucide-react";
 
-export default function DreamBuilding() {
+export default function DreamBuilding({
+  image,
+}: {
+  image: StaticImageData | string;
+}) {
   return (
     <section className="bg-secondary text-white">
       <div className="w-full h-full">
         <Image
-          src={dreamBuildingImg}
+          src={image}
           alt="dream steel building"
           className="object-cover w-full"
           priority
         />
       </div>
 
-      <Container fluid className="relative -mt-16 pb-16">
-        <div className="absolute w-full inset-x-0 top-0 h-40 -translate-y-6 mx-auto pointer-events-none rounded-t-lg bg-linear-to-t from-secondary to-secondary/95 blur-xl z-0" />
+      <Container fluid className="relative -mt-20 pb-20 px-0!">
+        <div className="absolute w-full top-0 h-40 -translate-y-12 x-auto  bg-linear-to-t from-secondary to-secondary/80 blur-sm z-0" />
 
-        <div className="relative mb-36 z-10 text-center">
-          <h2 className="text-3xl sm:text-4xl font-bold">
+        <div className="relative mb-32 mt-5 z-10 text-center">
+          <h2 className="text-2xl font-bold">
             Ready to Build Your Dream Steel Building?
           </h2>
-          <p className="mt-3 text-lg text-gray-200">
+          <p className="mt-3 text-xl text-gray-200">
             Get a free engineered drawing & quote in just 24 hours.
           </p>
         </div>
@@ -84,7 +88,7 @@ export default function DreamBuilding() {
               </label>
 
               <div className="pt-2 flex justify-center">
-                <Button className="rounded bg-secondary" type="submit">
+                <Button className="rounded bg-secondary min-w-40" type="submit">
                   Get My Quote
                   <ArrowRight />
                 </Button>
