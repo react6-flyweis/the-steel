b18eef3dfdd6711e0e472350e2f1b23e1f0dbd4a
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-15 22:21:52 +0530

    feat: refactor Why Us page l

--- app/why-us/page.tsx
+++ app/why-us/page.tsx
@@ -1,46 +1,23 @@
-import whyUsHero from "@/assets/why-us-hero.webp";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import DreamBuilding from "@/components/home/DreamBuilding";
+import OurPromise from "@/components/home/OurPromise";
+import Testimonials from "@/components/home/Testimonials";
+import WhyUsHero from "@/components/WhyUsHero";
+import WhyChooseComparison from "@/components/WhyChooseComparison";
+
+import dreamBuildingImg from "@/assets/dream-building-why-us.png";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Page() {
   return (
-    <section className="relative w-full h-[calc(100vh-80px)]">
-      <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: `url(${whyUsHero.src})` }}
-        aria-hidden
-      />
-
-      <div className="absolute bottom-0 w-full bg-linear-to-b h-1/2 from-transparent via-black/30 to-black/80" />
-
-      <div className="relative z-10 flex items-end justify-center h-full px-6 pb-12">
-        <div className="text-center">
-          <h1 className=" text-3xl  font-bold leading-tight text-white">
-            {" "}
-            <span className="text-blue-500">Why Thousands Trust</span>{" "}
-            <span className="text-white">The Steel</span>{" "}
-          </h1>
-          <h1 className=" text-3xl  font-bold leading-tight text-white">
-            Company{" "}
-            <span className="text-blue-500">
-              for Their Steel Building Projects
-            </span>
-          </h1>
-
-          <p className="mt-3 text-gray-200  max-w-2xl mx-auto">
-            We don’t just sell steel buildings—we engineer lasting solutions,
-            backed by expertise, service, and unmatched reliability.
-          </p>
-
-          <div className="mt-3 flex items-center justify-center gap-4">
-            <Button className=" bg-white border-primary text-primary hover:bg-gray-100 rounded w-40">
-              Get a Quote
-            </Button>
-
-            <Button className="rounded w-40">See Our Projects</Button>
-          </div>
-        </div>
-      </div>
-    </section>
+    <>
+      <Header />
+      <WhyUsHero />
+      <OurPromise />
+      <WhyChooseComparison />
+      <Testimonials />
+      <DreamBuilding image={dreamBuildingImg} />
+      <Footer />
+    </>
   );
 }
