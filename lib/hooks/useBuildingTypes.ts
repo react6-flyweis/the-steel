03a32fa95e614ed1b2db66d032892786e564792b
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-22 20:51:53 +0530

    feat: add useBuildingTypes hook for fetching building type data

---
+++ lib/hooks/useBuildingTypes.ts
@@ -0,0 +1,27 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/lib/api";
+
+export type BuildingType = {
+  _id: string;
+  title: string;
+  description: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
+
+export function useBuildingTypes() {
+  return useQuery<BuildingType[], unknown>({
+    queryKey: ["building-types"],
+    queryFn: async () => {
+      const res = await api.get("/static/getBuildingType");
+      return res.data?.data ?? [];
+    },
+    staleTime: 1000 * 60 * 5,
+  });
+}
+
+export default useBuildingTypes;
