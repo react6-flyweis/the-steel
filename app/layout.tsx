b43f96c6c576f70989450e79fdd50046314c0763
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-24 00:09:39 +0530

    feat: add new icon files and update layout metadata for improved branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,48 @@ export const metadata: Metadata = {
   title: "Steel Building Depot",
   description:
     "Engineered Steel Buildings Built for Strength, Style & Longevity.",
+  icons: [
+    {
+      url: "android_chrome_192X192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      url: "android_chrome_512X512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+    {
+      url: "apple_touch_icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+    {
+      url: "favicon_16X16.png",
+      sizes: "16x16",
+      type: "image/png",
+    },
+    {
+      url: "favicon_32X32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
+    {
+      url: "favicon_96X96.png",
+      sizes: "96x96",
+      type: "image/png",
+    },
+    {
+      url: "mstile_150X150.png",
+      sizes: "150x150",
+      type: "image/png",
+    },
+    {
+      url: "safari_pinned_tab.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+  ],
 };
 
 export default function RootLayout({
