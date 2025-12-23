72465ce8f355c2106ca004e0ae2d0f808c1fe383
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-23 13:19:58 +0530

    feat: refactor BaysTab component layout for improved responsiveness and structure

--- components/designer/BaysTab.tsx
+++ components/designer/BaysTab.tsx
@@ -17,8 +17,8 @@ export default function BaysTab({
   nextDisabled,
 }: TabNavProps) {
   return (
-    <div className="flex-1 flex h-full">
-      <div className="w-[360px] bg-secondary p-6 overflow-auto flex flex-col ">
+    <div className="flex-1 flex h-full flex-col md:flex-row">
+      <div className="order-2 md:order-1 w-full md:w-[360px] bg-secondary p-6 overflow-y-auto pb-28 md:pb-6 flex flex-col min-h-[45vh] md:min-h-0">
         <h2 className="text-lg font-semibold mb-2 text-white">Bays</h2>
 
         <div className="flex gap-2 mt-auto">
@@ -43,7 +43,7 @@ export default function BaysTab({
         </div>
       </div>
 
-      <div className="flex-1 relative">
+      <div className="order-1 md:order-2 flex-1 relative min-h-[45vh] md:min-h-0">
         <Image
           src={designerBaysImg}
           alt="Bays preview"
