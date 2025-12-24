85a76fca94a030011a87b99a4cb7e821ddd5d01a
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-24 13:15:33 +0530

    fix: adjust max-width and padding in OurMission component for better layout

--- components/OurMission.tsx
+++ components/OurMission.tsx
@@ -8,7 +8,7 @@ export default function OurMission() {
         <div className="text-center">
           <h2 className="text-3xl md:text-4xl font-bold">Our Mission</h2>
 
-          <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-8 max-w-5xl mx-auto ">
+          <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-8 max-w-6xl px-6 mx-auto ">
             <div className="p-8 bg-slate-700/60 rounded-md shadow-sm">
               <h3 className="text-xl font-semibold">Our Mission</h3>
               <p className="mt-4 text-sm text-slate-200 leading-relaxed">
