4e7ca38840b59b12a883ca74ce567efae4cd437d
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-24 13:14:31 +0530

    feat: implement dialog state management in GetAQuoteDialog and add onClose prop to BuildingForm

--- components/GetAQuoteDialog.tsx
+++ components/GetAQuoteDialog.tsx
@@ -1,12 +1,15 @@
-import { PropsWithChildren } from "react";
+"use client";
+
+import { PropsWithChildren, useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import BuildingForm from "./home/BuildingForm";
 import tinShed from "@/assets/tin-shed.png";
 import Image from "next/image";
 
 export default function GetAQuoteDialog({ children }: PropsWithChildren) {
+  const [open, setOpen] = useState(false);
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent className="sm:max-w-4xl p-0 rounded-xl border-0 h-[90vh] overflow-hidden">
         <div className="relative">
@@ -15,7 +18,7 @@ export default function GetAQuoteDialog({ children }: PropsWithChildren) {
             alt="Tin Shed"
             className="absolute -top-96 -left-68 opacity-15  -rotate-225 h-full object-cover rounded-r-xl"
           />
-          <BuildingForm isDialog={true} />
+          <BuildingForm isDialog={true} onClose={() => setOpen(false)} />
           <Image
             src={tinShed}
             alt="Tin Shed"
